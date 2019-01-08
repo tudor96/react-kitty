2db48b5a2720336e65d35b9e11354252fbaee9ae
tudor96 <gltudor96@gmail.com>
2019-01-08 17:26:54 +0200

    Updates

---
+++ precache-manifest.52621efa69ed07a974616a52d25128f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "70ffce1de758d9c337d3",
+    "url": "/react-kitty/static/js/runtime~main.70ffce1d.js"
+  },
+  {
+    "revision": "4e3d24b8b7337665502c",
+    "url": "/react-kitty/static/js/main.4e3d24b8.chunk.js"
+  },
+  {
+    "revision": "8a8ede18ec5e0ca4e725",
+    "url": "/react-kitty/static/js/1.8a8ede18.chunk.js"
+  },
+  {
+    "revision": "4e3d24b8b7337665502c",
+    "url": "/react-kitty/static/css/main.aec4ae55.chunk.css"
+  },
+  {
+    "revision": "8a8ede18ec5e0ca4e725",
+    "url": "/react-kitty/static/css/1.19462f68.chunk.css"
+  },
+  {
+    "revision": "bc639eb11e806c282221f921bd8f671a",
+    "url": "/react-kitty/index.html"
+  }
+];
